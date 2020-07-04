7e2f8b01c4efcc25819bf71d2b0a2297dae442ea
Ali Rahim Taleqani <36344134+Ali-RT@users.noreply.github.com>
2020-07-04 17:24:05 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,4 @@
-
-
-
-
-def fibonacci(n:int)-> int:
+def fibonacci_recursive(n:int) -> int:
     if n < 0:
         print(f"{n} is not a valid input.")
 
@@ -11,6 +7,21 @@ def fibonacci(n:int)-> int:
     elif n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
 
-#print(fibonacci(-1))
+
+def fibonacci_iterative(n:int) -> int:
+    if n < 0:
+        print(f"{n} is not a valid input.")
+
+    f = [0, 1]
+
+    for i in range(2,n+2):
+        f.append(f[i-1] + f[i-2])
+
+    return f[n]
+
+
+#print(fibonacci_recursive(9))
+
+#print(fibonacci_iterative(9))
